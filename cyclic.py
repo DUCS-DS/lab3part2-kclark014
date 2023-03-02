9efa89cdfc5ef5d1a83086b2e5ce81a71cd79753
= <=>
2023-03-02 16:55:53 -0600

    Finished

--- cyclic.py
+++ cyclic.py
@@ -1,14 +1,43 @@
 from llist import *
+from gencyclic import lst
 
 def llprint(lst, num):
     """print the first num terms of linked list lst"""
-    pass
+    node = lst.head
+    counter = 0
+    while counter < num and node.next:
+        print(node.val)
+        node = node.next
+        counter+=1 
+         
 
 def find_cycle(lst):
-    """return the start index and length of the cycle"""
-    pass
+    f = lambda node: node.next
+    
+    tortoise = f(lst.head) # f(x0) is the element/node next to x0.
+    hare = f(f(lst.head))
+    while tortoise != hare:
+        tortoise = f(tortoise)
+        hare = f(f(hare))
+
+    # Find the position μ of first repetition.    
+    mu = 0
+    tortoise = lst.head
+    while tortoise != hare:
+        tortoise = f(tortoise)
+        hare = f(hare)   # Hare and tortoise move at same speed
+        mu += 1
+ 
+    lam = 1
+    hare = f(tortoise)
+    while tortoise != hare:
+        hare = f(hare)
+        lam += 1
+ 
+    return lam, mu
 
 
 if __name__ == "__main__":
-
-    pass
+    tup = find_cycle(lst)
+    print(f"cycle start: {tup[0]}")
+    print(f" length: {tup[1]}")
